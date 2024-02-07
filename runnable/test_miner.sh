85b7859031deac4abcb48cf9b3db02f1ac589f90
chaoticAttractoor <115046984+chaoticAttractoor@users.noreply.github.com>
2024-02-07 23:12:20 +1000

    Update test_miner.sh

--- runnable/test_miner.sh
+++ runnable/test_miner.sh
@@ -7,15 +7,14 @@ delay=5
 while true; do
     echo "Attempting to run the Python script..."
     # Run the Python script
-cd /root/time-series-prediction-subnet/
+    cd /root/time-series-prediction-subnet/
 
 # Check if cd command was successful
-if [ $? -eq 0 ]; then
   # Activate the virtual environment
-  source /root/time-series-prediction-subnet/venv/bin/activate
-  
-  # Run the Python script with arguments
-  /usr/bin/python3 /root/time-series-prediction-subnet/neurons/miner.py --netuid 3 --subtensor.network test --wallet.name miner --wallet.hotkey default --logging.debug --axon.port 8911
+    source /root/time-series-prediction-subnet/venv/bin/activate
+    
+    # Run the Python script with arguments
+    /usr/bin/python3 /root/time-series-prediction-subnet/neurons/miner.py --netuid 3 --subtensor.network test --wallet.name miner --wallet.hotkey default --logging.debug --axon.port 8911
     # Check the exit status of the Python script
     if [ $? -eq 0 ]; then
         echo "Python script succeeded."
